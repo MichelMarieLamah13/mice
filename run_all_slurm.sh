af0f5176f8317b828fb6a9c92c45e476eca80320
mmlamah <lamahmichelmarie@gmail.com>
2023-11-23 15:42:20 +0100

    23 nov 2023 15:42

--- run_all_slurm.sh
+++ run_all_slurm.sh
@@ -11,6 +11,7 @@
 source /etc/profile.d/conda.sh
 conda activate mice
 
+wget  http://www.cs.cmu.edu/~glai1/data/race/RACE.tar.gz -O data/
 
 bash download_models.sh
 # python3 run_stage_two.py -task imdb -stage2_exp mice_binary -editor_path results/imdb/editors/mice/imdb_editor.pth
