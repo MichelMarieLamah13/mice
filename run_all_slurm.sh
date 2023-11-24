e1fd8f23ab0479fcbc8fe07ae4366f2e35aaf1e8
miguel <lamahmichelmarie@gmail.com>
2023-11-24 01:26:40 +0100

    23 nov 2023 01:26

--- run_all_slurm.sh
+++ run_all_slurm.sh
@@ -17,7 +17,10 @@ conda activate mice
 
 # bash download_models.sh
 
-python3 run_stage_two.py -task imdb -stage2_exp mice_binary -editor_path results/imdb/editors/mice/imdb_editor.pth
+# python3 run_stage_two.py -task imdb -stage2_exp mice_binary -editor_path results/imdb/editors/mice/imdb_editor.pth
+
+python3 -m pdb run_stage_two.py -task imdb -stage2_exp mice_binary -editor_path results/imdb/editors/mice/imdb_editor.pth
+
 # bash run_all.sh
 
 
